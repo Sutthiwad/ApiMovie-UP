b3e150e5255bc1ae98039e0fec0a4976de187e5e
Sutthiwad <samoiizalf@gmail.com>
2017-08-24 14:19:49 +0700

    first commit.

---
+++ index.js
@@ -0,0 +1,23 @@
+const mockMovie = require('./mock_movie')
+const Hapi = require("hapi")
+const server = new Hapi.Server()
+
+
+server.connection({ port: 3000, host: 'localhost' });
+server.route({
+    method: 'GET',
+    path: '/movie/{id}',
+    handler: function (request, reply) {
+        console.log(request.params.id)
+        //todo : query bd by id
+        reply(mockMovie);
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
